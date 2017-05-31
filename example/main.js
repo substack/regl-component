2381466802187e20bec09627df54337bd3f1e657
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-05-31 17:35:15 +0200

    works

---
+++ example/main.js
@@ -0,0 +1,62 @@
+var app = require('choo')()
+var html = require('choo/html')
+var rcom = require('../')(require('regl'))
+var anormals = require('angle-normals')
+
+var chart = require('conway-hart')
+var demos = [
+  fromMesh(rcom.create(), chart('mI')),
+  fromMesh(rcom.create(), chart('jT')),
+  fromMesh(rcom.create(), chart('djmeD')),
+  fromMesh(rcom.create(), chart('pO'))
+]
+
+app.route('/', function (state, emit) {
+  return html`<body>
+    ${rcom.render()}
+    ${demos.map(function (demo) {
+      return html`<div>
+        ${demo.render({ width: 400, height: 150 })}
+        <hr>
+      </div>`
+    })}
+  </body>`
+})
+app.mount('body')
+
+function fromMesh (rc, mesh) {
+  var draw = rc.regl({
+    frag: `
+      precision highp float;
+      varying vec3 vnorm;
+      void main () {
+        gl_FragColor = vec4(vnorm*0.5+0.5,1);
+      }
+    `,
+    vert: `
+      precision highp float;
+      attribute vec3 position, normal;
+      varying vec3 vnorm;
+      uniform float aspect;
+      void main () {
+        vnorm = normal;
+        gl_Position = vec4(position.xy*vec2(1,aspect)*0.3,position.z*0.1+0.1,1);
+      }
+    `,
+    uniforms: {
+      aspect: function (context) {
+        return context.viewportWidth / context.viewportHeight
+      }
+    },
+    attributes: {
+      position: mesh.positions,
+      normal: anormals(mesh.cells, mesh.positions)
+    },
+    elements: mesh.cells
+  })
+  rc.regl.frame(function () {
+    rc.regl.clear({ color: [0,0,0,1], depth: true })
+    draw()
+  })
+  return rc
+}
---
+++ index.js
@@ -0,0 +1,107 @@
+var Nano = require('nanocomponent')
+var defregl = require('deferred-regl')
+var mregl = require('./multi.js')
+
+module.exports = Root
+
+function Root (regl) {
+  if (!(this instanceof Root)) return new Root(regl)
+  Nano.call(this)
+  this.components = []
+  this._regl = regl
+}
+Root.prototype = Object.create(Nano.prototype)
+
+Root.prototype.create = function () {
+  var c = new Component()
+  this.components.push(c)
+  if (this._mregl) c._setMregl(this._mregl)
+  return c
+}
+
+Root.prototype._render = function () {
+  if (!this.element) {
+    this.element = document.createElement('canvas')
+  }
+  return this.element
+}
+
+Root.prototype._update = function () { return false }
+
+Root.prototype._load = function () {
+  this._mregl = mregl(this._regl, this.element)
+  for (var i = 0; i < this.components.length; i++) {
+    this.components[i]._setMregl(this._mregl)
+  }
+}
+
+Root.prototype._unload = function () {
+  this._mregl.destroy()
+}
+
+function Component (opts) {
+  var self = this
+  if (!(self instanceof Component)) return new Component(opts)
+  Nano.call(self)
+  if (!opts) opts = {}
+  self._elwidth = null
+  self._elheight = null
+  self._opts = opts
+  self.element = document.createElement('div')
+  self.element.style.display = 'inline-block'
+  if (opts.width) {
+    self.element.style.width = opts.width + 'px'
+    self._elwidth = opts.width
+  }
+  if (opts.height) {
+    self.element.style.height = opts.height + 'px'
+    self._elheight = opts.height
+  }
+  self.regl = defregl()
+  self._regl = null
+  self._mregl = null
+  self._mreglqueue = []
+}
+Component.prototype = Object.create(Nano.prototype)
+
+Component.prototype._getMregl = function (fn) {
+  if (this._mregl) fn(this._mregl)
+  else this._mreglqueue.push(fn)
+}
+Component.prototype._setMregl = function (mr) {
+  this._mregl = mr
+  for (var i = 0; i < this._mreglqueue.length; i++) {
+    this._mreglqueue[i](this._mregl)
+  }
+  this._mreglqueue = null
+}
+
+Component.prototype._update = function (props) {
+  return this._elwidth !== props.width
+    || this._elheight !== props.height
+}
+
+Component.prototype._render = function () {
+  if (this.props.width !== this._elwidth) {
+    this.element.style.width = this.props.width + 'px'
+    this._elwidth = this.props.width
+  }
+  if (this.props.height !== this._elheight) {
+    this.element.style.height = this.props.height + 'px'
+    this._elheight = this.props.height
+  }
+  this._parent = document.createElement('div')
+  return this.element
+}
+
+Component.prototype._load = function () {
+  var self = this
+  self._getMregl(function (mregl) {
+    self._regl = mregl(self.element)
+    self.regl.setRegl(self._regl)
+  })
+}
+
+Component.prototype._unload = function () {
+  this.regl.destroy()
+}
